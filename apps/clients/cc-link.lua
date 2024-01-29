81740be35bb348d38f8d423a7a4b57d1fa9a7f06
Kensaa <kensadevs@gmail.com>
2024-01-29 03:21:33 +0100

    first commit

---
+++ apps/clients/cc-link.lua
@@ -0,0 +1,268 @@
+local configFields = {
+    url = 'string',
+    fileIDs = 'table',
+    root = 'string',
+    entrypoint = 'string'
+}
+
+local wrongConfigMenu = {
+    prompt = 'Your config file is invalid, please choose an action',
+    options = {
+        {
+            name = 'Re-Run the setup script (will wipe current config)',
+            action = setup
+        },
+        {
+            name = 'Exit program and fix config manually',
+            action = function() end
+        }
+    }
+
+}
+
+function connect(url, retry)
+    retry = retry or 0
+    if retry > 5 then
+        print('Server is not reachable, shutting down.')
+        os.shutdown()
+    end
+    local ws = http.websocket(url)
+    if not ws then
+        print('Unable to connect to server, retrying in 5 seconds')
+        sleep(5)
+        return connect(url, retry + 1)
+    else
+        print('Connected !')
+        return ws
+    end
+end
+
+function setup()
+    term.clear()
+    term.setCursorPos(1,1)
+    print("Welcome to CC-link setup script !")
+    print("Please note that this script will delete any startup.lua file already existing on this computer")
+    print("Press Enter to continue")
+    read()
+    print("Please enter the url of the server you want to connect to")
+    local url = removeEndSlash(stringInput(1)[1])
+    print("Please enter the ids of the files you want to sync")
+    local fileIDs = stringToNumberArray(stringInput())
+    print("Please enter the root directory of the files you want to sync, leave empty for the root directory of the computer")
+    local root = removeEndSlash(stringInput(1)[1])
+    print("Please enter the file you want to to be ran at startup, leave empty for auto-detection (startup.lua or the first lua file in the root directory)")
+    local entrypoint = removeEndSlash(stringInput(1)[1])
+    local config = {
+        url = url,
+        fileIDs = fileIDs,
+        root = root,
+        entrypoint = entrypoint
+    }
+    local configRaw = textutils.serializeJSON(config)
+    fs.open('./cc-link.conf', 'w').write(configRaw)
+
+    if not fs.exists(root) then
+        fs.makeDir(root)
+    end
+
+    if(fs.exists('./startup.lua')) then
+        fs.delete('./startup.lua')
+    end
+    shell.run('wget '..url..'/clients/cc-link.lua startup.lua')
+    os.reboot()
+end
+
+function sync(config)
+    local ws = connect(httpToWsAddress(config['url']))
+    ws.send(textutils.serializeJSON({
+        type = 'register',
+        fileIDs = config['fileIDs']
+    }))
+
+    local root = config['root']
+    local entrypoint = config['entrypoint']
+
+    if entrypoint == '' then
+        local files = fs.list(root)
+        -- if there is a startup_0.lua file, use it as entrypoint
+        for i, file in ipairs(files) do
+            if file == 'startup_0.lua' then
+                entrypoint = file
+                break
+            else
+                local fileExt = string.sub(file, -4)
+                if fileExt == '.lua' then
+                    if file ~= 'startup.lua' then
+                        entrypoint = file
+                        break
+                    end
+                end
+            end
+        end
+    end
+
+    if entrypoint == '' then
+        print('No file suitable for entrypoint found, still waiting for updates')
+    else
+        local entrypointPath = root..'/'..entrypoint
+        print('Starting entrypoint at '..entrypointPath)
+        local tabID = shell.openTab(root..'/'..entrypoint)
+        shell.switchTab(tabID)
+        print("Entrypoint started")
+    end
+    
+    
+    print('waiting for updates...')
+    while true do
+        local responseString, isBinary = ws.receive()
+        if not isBinary then
+            local response = textutils.unserializeJSON(responseString)
+            if response['type'] == 'fileUpdate' then
+                print('Received update')
+                local files = response['files']
+
+                local lastUpdate = {}
+                if(fs.exists('./cc-link.lastUpdate')) then
+                    local lastUpdateString = fs.open('./cc-link.lastUpdate', 'r')
+                    lastUpdate = textutils.unserializeJSON(lastUpdateString.readAll())
+                end
+
+                local fileUpdated = false
+                for i, file in ipairs(files) do
+                    local id = file['id']
+                    local name = file['name']
+                    local content = file['content']
+                    local updatedAt = file['updatedAt']
+
+                    if lastUpdate[id] ~= nil and lastUpdate[id] == updatedAt then
+                        print('File '..name..' is already up to date')
+                    else
+                        print('Updating file '..name..'...')
+
+                        if name == 'startup.lua' or name == 'startup' then
+                            name = 'startup_0.lua'
+                        end
+
+                        local path = root..'/'..name
+
+                        local fileHandle = fs.open(path, 'w')
+                        for i,line in ipairs(splitLines(content)) do
+                            fileHandle.writeLine(line)
+                        end
+                        fileHandle.close()
+                        fileUpdated = true
+                        lastUpdate[id] = updatedAt
+                    end
+                end
+                print('Update finished')
+                if fileUpdated then
+                    local lastUpdateString = textutils.serializeJSON(lastUpdate)
+                    fs.open('./cc-link.lastUpdate', 'w').write(lastUpdateString)
+                    print('Rebooting...')
+                    os.reboot()
+                end
+            end
+        end
+    end
+
+end
+
+function showMenu(menu)
+    local prompt = menu['prompt']
+    local options = menu['options']
+    while true do
+        print(prompt)
+        for i, option in ipairs(options) do
+            print(i .. '. ' .. option['name'])
+        end
+        local input = tonumber(read())
+        if input == nil or input < 1 or input > #options then
+            print('Invalid input, please try again')
+        else
+            term.clear()
+            term.setCursorPos(1,1)
+            local passed, returnValue = pcall(options[input]['action'])
+            if not passed then
+                print('An error has occured while executing action: ' .. returnValue)
+            else 
+                return returnValue
+            end
+        end
+    end
+end
+
+function stringInput(count)
+    local output = {}
+    if count ~= nil then
+        for i = 1, count do
+            table.insert(output, read())
+        end
+    else
+        while true do
+            local input = read()
+            if input == '' then
+                break
+            end
+            table.insert(output, input)
+        end
+    end
+
+    return output
+end
+
+function stringToNumberArray(arr)
+    local output = {}
+    for i, v in ipairs(arr) do
+        local num = tonumber(v)
+        if num ~= nil then
+            table.insert(output, tonumber(v))
+        end
+    end
+    return output
+end
+
+function removeEndSlash(string)
+    if string.sub(string, -1) == '/' then
+        return string.sub(string, 1, -2)
+    else
+        return string
+    end
+end
+
+function httpToWsAddress(url)
+    return 'ws'..string.sub(url, 5)
+end
+
+function splitLines(str)
+    local lines = {}
+    for s in string.gmatch(str,"[^\r\n]+") do
+        table.insert(lines, s)
+    end
+    return lines
+end
+
+
+
+if fs.exists('./cc-link.conf') then
+    local configRaw = fs.open('./cc-link.conf', 'r').readAll()
+    local config = textutils.unserializeJSON(configRaw)
+
+    local isConfigValid = true
+    for k, v in pairs(configFields) do
+        if config[k] == nil then
+            print('Missing field ' .. k .. ' in config file')
+            isConfigValid = false
+        elseif type(config[k]) ~= v then
+            print('Field ' .. k .. ' is not of type ' .. v)
+            isConfigValid = false
+        end
+    end
+
+    if isConfigValid then
+        sync(config)
+    else
+        showMenu(wrongConfigMenu)
+    end
+else
+    setup()
+end
---
+++ apps/server/src/server.ts
@@ -0,0 +1,143 @@
+import { PrismaClient } from '@prisma/client'
+import express from 'express'
+import cors from 'cors'
+import { createServer } from 'http'
+import ws from 'ws'
+import { z } from 'zod'
+import path from 'path'
+import fs from 'fs'
+
+const PORT = parseInt(process.env.SERVER_PORT || '7541')
+const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+interface Computer {
+    ws: ws.WebSocket
+    fileIDs: number[]
+}
+
+const computers: Computer[] = []
+
+const wsMessagesSchema = z.discriminatedUnion('type', [
+    z.object({
+        type: z.literal('register'),
+        fileIDs: z.number().array()
+    })
+])
+
+;(async () => {
+    const prisma = new PrismaClient()
+    const app = express()
+    app.use(express.json())
+    app.use(cors({ origin: '*' }))
+    const httpServer = createServer(app)
+    const wsServer = new ws.Server({ server: httpServer })
+
+    httpServer.listen(PORT, () => console.log(`listening on port ${PORT}`))
+
+    wsServer.on('connection', ws => {
+        console.log('new websocket connection')
+
+        ws.on('close', () => {
+            console.log('websocket connection closed')
+            computers.splice(
+                computers.findIndex(c => c.ws === ws),
+                1
+            )
+        })
+
+        ws.on('message', async data => {
+            const parseResult = wsMessagesSchema.safeParse(JSON.parse(data.toString()))
+            if (!parseResult.success) {
+                //received invalid message
+                ws.send(JSON.stringify({ type: 'error', message: parseResult.error.message }))
+                return
+            }
+            const { data: message } = parseResult
+
+            switch (message.type) {
+                case 'register':
+                    const files = await prisma.file.findMany({
+                        where: {
+                            id: {
+                                in: message.fileIDs
+                            }
+                        }
+                    })
+                    for (const fileID of message.fileIDs) {
+                        if (!files.find(f => f.id === fileID)) {
+                            ws.send(JSON.stringify({ type: 'error', message: `File with id ${fileID} does not exist` }))
+                            return
+                        }
+                    }
+                    computers.push({ ws, fileIDs: message.fileIDs })
+                    await wait(500)
+                    ws.send(JSON.stringify({ type: 'fileUpdate', files }))
+                    break
+            }
+        })
+    })
+
+    app.post('/file', async (req, res) => {
+        const schema = z.object({
+            id: z.number().optional(),
+            name: z.string(),
+            content: z.string()
+        })
+
+        const parseResult = schema.safeParse(req.body)
+        if (!parseResult.success) {
+            res.status(400).send(parseResult.error.message)
+            return
+        }
+        const { name, content, id } = parseResult.data
+
+        if (id) {
+            //id in request ---> update file
+            let file = await prisma.file.findUnique({
+                where: {
+                    id
+                }
+            })
+            // chech if file exists and if it does, update it
+            if (file) {
+                file = await prisma.file.update({
+                    where: {
+                        id
+                    },
+                    data: {
+                        name,
+                        content
+                    }
+                })
+
+                // send update to all computers
+                for (const computer of computers) {
+                    if (computer.fileIDs.includes(id)) {
+                        computer.ws.send(JSON.stringify({ type: 'fileUpdate', files: [file] }))
+                    }
+                }
+
+                res.status(200).send({ id: file.id, name: file.name })
+                return
+            }
+            // else create the file like if no id was provided
+        }
+        // no id in request ---> create file
+        const file = await prisma.file.create({
+            data: {
+                name,
+                content
+            }
+        })
+        res.status(200).send({ id: file.id, name: file.name })
+    })
+
+    const CLIENTS_PATH = path.resolve(
+        process.env.NODE_ENV === 'production' ? './clients/' : path.join(__dirname, '..', '..', 'clients/')
+    )
+    console.log('clients folder :', CLIENTS_PATH)
+    app.get('/', (req, res) => {
+        const file = fs.readFileSync(path.join(CLIENTS_PATH, 'cc-link.lua'), 'utf-8')
+        res.status(200).send(file)
+    })
+})()
